e5eba5b182b936e15dabf84695edc557f39a8536
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-05 10:31:44 +0700

    add datatypes training

--- pre-test/day-1/HelloWorld/src/Main.java
+++ pre-test/day-1/HelloWorld/src/Main.java
@@ -3,5 +3,27 @@ public class Main {
 
         System.out.println("Hello world!");
 
+        String myName;
+        myName = "Kevin";
+        Integer myAge;
+        myAge = 25;
+        Float mySalary;
+        mySalary = 50000.00f;
+        double myFavNumber;
+        myFavNumber = 5.00;
+        Long myTall;
+        myTall = 2L;
+        char myGender;
+        myGender = 'm';
+        Boolean myHappy;
+        myHappy = true;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(mySalary);
+        System.out.println(myTall);
+        System.out.println(myGender);
+        System.out.println(myHappy);
+        System.out.println(myFavNumber);
     }
 }
