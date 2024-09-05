f5cfa2cbba094cee86723024c16c3c9e880f4cd5
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-05 14:31:42 +0700

    add multiple employee and prints it out

--- exercise/day-1/section-2/my-employee/src/Main.java
+++ exercise/day-1/section-2/my-employee/src/Main.java
@@ -1,13 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        Employee myEmployee = new Employee();
+        Employee myEmployee1 = new Employee();
+        myEmployee1.name = "Kevin";
+        myEmployee1.npk = "20240001";
+        myEmployee1.salary = 10000;
+        myEmployee1.address = "TB Simatupang 1";
+        myEmployee1.age = 50;
+        System.out.println(myEmployee1.name);
+        System.out.println(myEmployee1.npk);
 
-        myEmployee.name = "Kevin";
-        myEmployee.npk = "20240001";
-        myEmployee.salary = 10000;
-        myEmployee.address = "TB Simatupang";
-        myEmployee.age = 50;
+        Employee myEmployee2 = new Employee();
+        myEmployee2.name = "Taufik";
+        myEmployee2.npk = "20240002";
+        myEmployee2.salary = 5000;
+        myEmployee2.address = "TB Simatupang 2";
+        myEmployee2.age = 55;
+        System.out.println(myEmployee2.name);
+        System.out.println(myEmployee2.npk);
 
-        myEmployee.sayHello();
+        Employee myEmployee3 = new Employee();
+        myEmployee3.name = "Taufik sabar";
+        myEmployee3.npk = "20240003";
+        myEmployee3.salary = 6000;
+        myEmployee3.address = "TB Simatupang 3";
+        myEmployee3.age = 56;
+        System.out.println(myEmployee3.name);
+        System.out.println(myEmployee3.npk);
+
+        Employee myEmployee4 = new Employee();
+        myEmployee4.name = "Zul Java";
+        myEmployee4.npk = "20240004";
+        myEmployee4.salary = 8000;
+        myEmployee4.address = "TB Simatupang 4";
+        myEmployee4.age = 49;
+        System.out.println(myEmployee4.name);
+        System.out.println(myEmployee4.npk);
+
+        Employee myEmployee5 = new Employee();
+        myEmployee5.name = "Zul Java kotlin";
+        myEmployee5.npk = "20240005";
+        myEmployee5.salary = 2000;
+        myEmployee5.address = "TB Simatupang 5";
+        myEmployee5.age = 32;
+        System.out.println(myEmployee5.name);
+        System.out.println(myEmployee5.npk);
+
     }
 }
