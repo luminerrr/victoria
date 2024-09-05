aae68916909ac2e6c35a996ae88993bd9ae4ff2e
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-05 15:47:45 +0700

    add bank class and proof

---
+++ exercise/day-1/section-2/my-love/src/BankAccount.java
@@ -0,0 +1,33 @@
+public class BankAccount {
+    public String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount() {
+        accountNumber = "";
+        accountHolder = "";
+        balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if(amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+}
---
+++ exercise/day-1/section-2/my-love/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount myBank = new BankAccount();
+
+        myBank.deposit(10_000);
+        myBank.setAccountHolder("Markus Horison");
+
+        myBank.withdraw(2_000);
+
+        System.out.println("This account belongs to: " + myBank.getAccountHolder());
+        System.out.println("This account has this much money: " + myBank.getBalance());
+    }
+}
