1bc324a189fef42a96f7abb127112612925f665c
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-05 14:05:44 +0700

    add car class

---
+++ exercise/day-1/section-2/MyCar/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Car myCar = new Car();
+        myCar.model = "Toyota";
+        myCar.year = 2020;
+
+        myCar.drive();
+    }
+}
+
